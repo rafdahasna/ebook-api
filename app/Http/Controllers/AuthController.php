86794ae6f0bf333d621682e44475790c5d9c1164
Rafda Hasna Imamah <3103119149@student.smktelkom-pwt.sch.id>
2021-09-28 01:11:10 -0700

    migration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,15 +6,87 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function me(){
-        return
-        [
-            "NISN" => 3103119149,
-            "Name" => "Rafda Hasna Imamah",
-            "Gender" => "Female",
-            "Phone" => 6281226045779,
-            "Class" => "XII RPL 5"
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function me()
+    {
+        $biodata = [
+            "nis"=>'3103119149',
+            "name"=>'Rafda Hasna Imamah',
+            "phone"=>'081226045779',
+            "gender"=>'Perempuan',
+            "class"=>'XII RPL 5'
         ];
+
+        return $biodata;
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
 
 /*
@@ -16,7 +16,13 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::get('/me', [AuthController::class, 'me']);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-
-
+Route::get('/book', [BookController::class, 'index']);
+Route::post('/book', [BookController::class, 'store']);
+Route::get('/book/{id}', [BookController::class, 'show']);
+Route::put('/book/{id}', [BookController::class, 'update']);
+Route::delete('/book/{id}', [BookController::class, 'destroy']);
+Route::resource('book', BookController::class)->except('edit', 'create');
